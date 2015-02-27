45197cb139b79d79bcc7ee16bb993bc023421637
thomazpicelli <thomazpicelli@gmail.com>
2015-02-27 18:20:25 -0300

    basico Funci, Atend, Gerente

---
+++ LinguagemProgramacao2/src/java/com/br/lp2/model/Atendente.java
@@ -0,0 +1,12 @@
+package com.br.lp2.model;
+
+import java.io.Serializable;
+
+/**
+ * @version 1.0
+ * @author thomazpicelli
+ */
+public class Atendente extends Funcionario implements Serializable{
+    private int pk;
+    
+}
---
+++ LinguagemProgramacao2/src/java/com/br/lp2/model/Funcionario.java
@@ -0,0 +1,11 @@
+package com.br.lp2.model;
+
+import java.io.Serializable;
+
+/**
+ * @version 1.0
+ * @author thomazpicelli
+ */
+public class Funcionario implements Serializable{
+    private int pk;
+}
